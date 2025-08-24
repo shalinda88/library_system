42618cbe1892e08df46ccf3fc8308cf83834afb8
shalinda88 <raviharajayasiri@gmail.com>
2025-08-24 21:09:34 +0530

    ci cd yaml file again refectered

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -19,8 +19,8 @@ const config: Config = {
   MONGO_URI: process.env.MONGODB_CONNECTION_STRING  || 'mongodb://localhost:27017/library_system',
   JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret_key',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
-  CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:3000',
-  SOCKET_CORS_ORIGIN: process.env.SOCKET_CORS_ORIGIN || 'http://localhost:3000'
+  CLIENT_URL: process.env.CLIENT_URL || 'https://moonlit-griffin-467aa4.netlify.app',
+  SOCKET_CORS_ORIGIN: process.env.SOCKET_CORS_ORIGIN || 'https://moonlit-griffin-467aa4.netlify.app'
 };
 
 export default config;
